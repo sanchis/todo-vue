8899a7d028ad0f0bce10619004505e729296af43
sanchis <sanchis1992@gmail.com>
2021-05-15 19:17:56 +0000

    Deploying to gh-pages from  @ 8070c33fad37976b30434584ca7b6d839fde7482 🚀

---
+++ js/about.3717a802.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Adrián Sanchis Gallego")]),n("h2",[t._v(" Get started with Vue.js")])])}],l=n("2877"),r={},u=Object(l["a"])(r,a,s,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.3717a802.js.map
